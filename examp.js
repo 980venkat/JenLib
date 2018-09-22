46b5a7849ffc7275fa271fa66a9c05bdf07b77f0
980venkat <980venkat@gmail.com>
2018-09-21 22:48:32 -0400

    Create examp.js

---
+++ examp.js
@@ -0,0 +1,20 @@
+'use strict';
+
+
+module.exports = {
+    ra: {
+        basePath: '/edgepoint/',
+        port: 8081,
+        username: 'oneuser',
+        password: 'oneuser',
+        host: 'localhost'
+    },
+    sh: {
+        basePath: '/edgepoint/',
+        port: 8080,
+        username: 'oneuser',
+        password: 'oneuser',
+        host: 'localhost'
+    }
+   
+};
